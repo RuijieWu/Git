00cbec1795232d6bfd51502b2587f87254afffe7
JeRyWu <1365840492@qq.com>
2022-09-21 11:41:07 +0800

    Test

---
+++ Git/Test.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: JeRyWu 1365840492@qq.com
+ * @Date: 2022-09-21 11:20:12
+ * @LastEditors: JeRyWu 1365840492@qq.com
+ * @LastEditTime: 2022-09-21 11:38:35
+ * @FilePath: /Git/Test.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<stdio.h>
+int main()
+{
+    printf("HelloWorld!");
+    return 0;
+}
